f5dcd3ebfc0b74de9cdcbab24889027e3e1999f3
Ahmad Rifa'i <arifai209@gmail.com>
2020-05-31 11:25:39 +0700

    first commit

---
+++ database/migrations/2020_05_31_041344_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('chat_id');
+            $table->string('firstname', 128);
+            $table->string('lastname', 128);
+            $table->string('username', 128);
+            $table->decimal('balance');
+            $table->decimal('point');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
